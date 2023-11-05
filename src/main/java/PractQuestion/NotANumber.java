1255603882f3772db1fd0f8f0506cb1e1ff29a42
KabindraAutomation <kabindora@gmail.com>
2023-11-05 16:41:15 -0600

    WAP for NaN

---
+++ src/main/java/PractQuestion/NotANumber.java
@@ -0,0 +1,18 @@
+package PractQuestion;
+
+public class NotANumber {
+    public static void main(String[] args) {
+        /**
+         * What is NaN - Not a Number?
+         * How is NaN defined in different language
+         * Java .....
+         *
+         */
+
+        //System.out.println(2/0);
+        System.out.println(2.0/0.0);
+        System.out.println(0.0/0.0);
+        System.out.println(Math.sqrt(-1));
+
+    }
+}
---
+++ src/main/java/PractQuestion/TestNum.java
@@ -0,0 +1,20 @@
+package PractQuestion;
+
+public class TestNum {
+    public static void main(String[] args) {
+
+        /**
+         * compare two int number(Integer Caching)
+         *  Range is -128 to 127
+         */
+        Integer num1 = 190;
+        Integer num2 = 190;
+
+        if(num1==num2) {
+            System.out.println("Both are equal");
+        }
+        else {
+            System.out.println("both are not equal");
+        }
+    }
+}
